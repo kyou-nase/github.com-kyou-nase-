204507e7a4ac6ba6c1fd8b63fd8eb6332fba8e29
kyou <67418265+kyou-nase@users.noreply.github.com>
2022-07-23 18:05:30 +0800

    Turns on proxy

---
+++ Mihoyo/Grasscutter Documentations/Troubleshoot/proxy_on.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d address:portNumber /f
