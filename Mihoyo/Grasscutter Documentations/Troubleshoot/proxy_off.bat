adb00b5c1274ff31614974b11a839774005d9238
kyou <67418265+kyou-nase@users.noreply.github.com>
2022-08-05 19:56:58 +0800

    Delete Mihoyo/Grasscutter Documentations directory

--- Mihoyo/Grasscutter Documentations/Troubleshoot/proxy_off.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
--- Mihoyo/Grasscutter Documentations/Troubleshoot/proxy_on.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d address:portNumber /f
